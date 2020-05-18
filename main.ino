d52a6363710b82fabd05f611b3fcbae02bedcdae
reanzi <nziku99@gmail.com>
2020-05-18 17:43:09 +0300

    First commits

---
+++ main.ino
@@ -0,0 +1,38 @@
+/* A memory game for childrens */
+// Variables
+int grnBtn = 2,
+    ylwBtn = 3,
+    redBtn = 4,
+    bluBtn = 5,
+    okBtn = 6,
+    upBtn = 7,
+    dwnBtn = 8,
+    rstBtn = 9,
+    buzeer = 10,
+    grnLED = 11,
+    ylwLED = 12,
+    redLED = 13,
+    bluLED = 14;
+int level = 0;
+int LEDS = 1;
+
+void setup()
+{
+}
+
+void loop()
+{
+    // main loop
+}
+
+// logics and functions
+
+int numberGen(int level)
+{
+    // generate random number, whicha are the number of led's blinks depending on the level
+}
+
+void blink(int speed)
+{
+    // this will blink individual led as instructed
+}
